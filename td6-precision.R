abae767dc7bb842dc53363b58412b046b5508bcf
yanndav <yann.collindavid@gmail.com>
2020-11-28 10:08:14 +0100

    td8 - update final

--- td6-precision.R
+++ td6-precision.R
@@ -26,3 +26,10 @@ reg4 = lm(Yield ~ `Urban Pop` + FRA + `Urban Pop` * FRA, data=base_sub )
 summary(reg4)
 
 summary(reg3)
+
+file = read.table('C:/Users/yann1/Downloads/GREEN_GROWTH_28112020091820726.csv', head = TRUE, sep=",")
+saveRDS(file, 'C:/Users/yann1/Downloads/GREEN_GROWTH.RDS')
+
+read_csv('C:/Users/yann1/Downloads/GREEN_GROWTH.csv',quote = "\"\\\"")
+nameLine = readLines('C:/Users/yann1/Downloads/GREEN_GROWTH.csv', n=1)
+fileColNames <- unlist(strsplit(nameLine,","))
